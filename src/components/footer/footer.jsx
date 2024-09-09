352e234cb4205145b63308bfad1f67deb803b459
patilmahesh007 <patilmahesh4007>
2024-09-09 10:11:23 +0530

    images folder created

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,19 @@
 import './footer.css';
-
+import instaLogo from './../../images/footer/instagram.png';
+import fbLogo from './../../images/footer/social-media.png';
+import xLogo from './../../images/footer/twitter.png';
 function Footer() {
   return (
     <footer>
-    <div className="content">all rights reserved by mahesh </div>
+      <div className="content">
+        <div className="copyright-text" >Copyright &copy; 2023  by Agrimart</div>
+        <div className="footer-socials">
+          <a><img src={instaLogo} className="footer-socials-img" /></a>
+          <a><img src={fbLogo} className="footer-socials-img" /></a>
+          <a><img src={xLogo} className="footer-socials-img" /></a>
+
+        </div>
+      </div>
     </footer>
   );
 }
