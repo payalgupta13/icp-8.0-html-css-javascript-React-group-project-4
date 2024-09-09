0d23bf39192c8e7c6d88ba55acb92a47adbd4fa8
22piyush <piyushaglawe05@gmail.com>
2024-09-09 15:02:46 +0530

    blog page

--- src/view/Blog/Blog.js
+++ src/view/Blog/Blog.js
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import { BlogHeading, TagLine, TagLine_2, Blogs } from "../../config/BlogData";
 import "./Blog.css";
 import BlogCard from "../../components/BlogCards/BlogCard";
 
 function Blog() {
-
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   return (
@@ -12,37 +11,26 @@ function Blog() {
       <div className="blog-page">
         <div className="blog-container">
           <h1>{BlogHeading}</h1>
-          <p>{TagLine}</p>
-          <p>{TagLine_2}</p>
+          <p className="para">{TagLine}</p>
+          <p className="para">{TagLine_2}</p>
         </div>
-
 
         <div className="btn-container">
-        <button onClick={() => setIsPopupOpen(true)}>Add Blog</button>
+          <button onClick={() => setIsPopupOpen(true)}>Add Blog</button>
 
-{isPopupOpen && (
-  <div className="popup-overlay">
-    <div className="popup-content">
-      <h2>Add a New Blog</h2>
-      <form>
-        <label>
-          Title:
-          <input type="text" name="title" />
-        </label>
-        <br />
-        <label>
-          Content:
-          <textarea name="content" />
-        </label>
-        <br />
-        <button type="submit">Submit</button>
-        <button type="button" onClick={() => setIsPopupOpen(false)}>Close</button>
-      </form>
-    </div>
-  </div>
-)}
+          {isPopupOpen && (
+            <div className="popup-overlay">
+              <div className="popup-content">
+                <h2>Add a New Blog</h2>
+                <input type="text" />
+                <button type="submit">Submit</button>
+                <button type="button" onClick={() => setIsPopupOpen(false)}>
+                  Close
+                </button>
+              </div>
+            </div>
+          )}
         </div>
-
 
         <div className="component-cards">
           {Blogs.map((BlogCards) => {
