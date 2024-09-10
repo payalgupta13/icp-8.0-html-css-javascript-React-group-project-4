e4718099b6f8a8cdcd5e6f7eb6adf8e0ded37f2e
22piyush <piyushaglawe05@gmail.com>
2024-09-10 17:36:31 +0530

    done

--- src/components/BlogCards/BlogCard.js
+++ src/components/BlogCards/BlogCard.js
@@ -7,11 +7,11 @@ function BlogCard({id, imgUrl, title, description, details}) {
   return (
     <div className="blog-card-containers">
       <img src={imgUrl} className="blog-card-image" alt={title} />
-      <div className="blog-card-content">
+      <Link to={`/blog/${id}`}> <div className="blog-card-content">
         <h3 className="blog-card-title">{title}</h3>
         <p className="blog-card-desc">{description}</p>
         <h5 className="blog-card-details">{details}</h5>
-      </div>
+      </div></Link>
       <div>
         <hr/>
        <Link to={`/blog/${id}`}><button className="read-more-btn">Read More</button></Link>
