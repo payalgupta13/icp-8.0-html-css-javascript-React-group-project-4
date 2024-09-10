db2414527ef4e7357309f6f821854686cc3bb6f8
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-10 22:33:44 +0530

    page changed

--- src/view/services/Services.jsx
+++ src/view/services/Services.jsx
@@ -2,17 +2,13 @@ import React from 'react'
 import Footer from "../../components/footer/footer"
 import Navbar from "../../components/navbar/nav"
 import './service.css'
-import MainImg from './../../images/service/agri image.jpg'
+
 function Services() {
   return (
     <div>   
       <Navbar/>
-      <div className='service-main-container'>
-        <div className='service-img-container'>
-           <img className='service-img' src={MainImg} />
-            <h2 className='service-heading'> Our Service</h2>
-        </div>
-      </div>
+      
+     
       <Footer/>
     </div>
   )
