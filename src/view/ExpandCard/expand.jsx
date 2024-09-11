6a81fe0bfe3919774bf8f26e696d21fe854e12e9
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-11 19:11:25 +0530

    quality counter adding expand page

--- src/view/ExpandCard/expand.jsx
+++ src/view/ExpandCard/expand.jsx
@@ -1,7 +1,12 @@
+import { useState } from "react"
 import Footer from "../../components/footer/footer"
 import Navbar from "../../components/navbar/nav"
 import './expand.css'
 function expand() {
+
+  const [price , setprice]=useState()
+  const [selectprice , setSelectprice] =useState()
+  const [kgprice , setkgprice] =useState()
   return (
     <>
     <Navbar/>
@@ -9,15 +14,20 @@ function expand() {
              <div>
               <img  src={"https://www.freepngimg.com/thumb/categories/2970.png"}/>
              </div>
-             <p>0.5kg</p>
+             
              <div className="expand-details">
                <h2>Cabbage</h2>
+               <p onChange={(e)=>setSelectprice(selectprice)}>{selectprice}</p>
                <span> price : ₹30</span>
-                <select>
-                    <option>  500 Gms</option>
+                <select  value={selectprice} >
+                    <option> 500 Gms</option>
                     <option> 1 kg</option>
                     <option> 2 kg</option>
                 </select>
+
+                <div className="expand-quantity-counter">
+                       1
+                </div>
              </div>
           </div>
     <Footer/>
