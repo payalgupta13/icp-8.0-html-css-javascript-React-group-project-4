629905a1bb3c3198bf9af4e17d322e8b3f857bc0
patilmahesh007 <patilmahesh4007>
2024-09-11 17:46:22 +0530

    card3 updated

--- src/components/homepageCard3/card3.jsx
+++ src/components/homepageCard3/card3.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import './card3.css';
-function Card3card(Image) {
+function Card3card({ image, description }) {
   return (
-<div className='App-section-3'>
-    <span>
-        <h2>Fresh Food</h2>
-        <p>Healthy Food</p>
-        <h3>Get special discount on <br/>selected products</h3>
-    </span>
-    <img className='App-section-img' src={Image}/>
-    <Link><div className='App-section-3-shop'>Know More</div></Link>
+        <div className='App-section-3'>
+        <div className='App-section-3-div1'>
+          <span>
+            <h2>Fresh Food</h2>
+            <p>Healthy Food</p>
+            <h3>Get special discount on <br />{description}</h3>
+          <Link><div>Know More</div></Link>
+          </span>
+        </div>
+        <div className='App-section-3-div2'>
+          <img className='App-section-img' src={image} />
 
-</div>
+        </div>
+      </div>
   );
 }
 
--- src/config/card3.js
+++ src/config/card3.js
@@ -1,15 +1,23 @@
-import straw from "./../images/card3/straw.png"
-import brocoli from "./../images/card3/brocoli.png"
+import straw from "./../images/card3/straw.png";
+import brocoli from "./../images/card3/brocoli.png";
+import lettuce from "./../images/card3/lettuce.png";
 
 const card3 = [
-    {
-        Image: {brocoli},
-    },
-    {
-        Image: {straw}
-        
-    }
-    
-]
+  {
+    image: brocoli,
+    title: "Broccoli",
+    description: "A healthy vegetable"},
+  
+  {
+    image: straw,
+    title: "Strawberry",
+    description: "A delicious fruit",
+  },
+  {
+    image: lettuce,
+    title: "Strawberry",
+    description: "A delicious fruit",
+  },
+];
 
-export default card3
+export default card3;
