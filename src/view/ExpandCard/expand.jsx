efe43041a9cffa061d439fe8ce9bfce246d86773
Tusharzade2002 <tusharzade29@gmail.com>
2024-09-11 19:39:30 +0530

    quality counter added

--- src/view/ExpandCard/expand.jsx
+++ src/view/ExpandCard/expand.jsx
@@ -2,11 +2,14 @@ import { useState } from "react"
 import Footer from "../../components/footer/footer"
 import Navbar from "../../components/navbar/nav"
 import './expand.css'
+import minus  from './../../images/expand/minus-icon.png'
+import plus from './../../images/expand/plus icon.png'
 function expand() {
 
   const [price , setprice]=useState()
   const [selectprice , setSelectprice] =useState()
   const [kgprice , setkgprice] =useState()
+  const [quality , setquality]=useState(1)
   return (
     <>
     <Navbar/>
@@ -19,14 +22,21 @@ function expand() {
                <h2>Cabbage</h2>
                <p onChange={(e)=>setSelectprice(selectprice)}>{selectprice}</p>
                <span> price : ₹30</span>
-                <select  value={selectprice} >
+            unit
+                <select  value={selectprice} className="expand-select">
                     <option> 500 Gms</option>
                     <option> 1 kg</option>
                     <option> 2 kg</option>
                 </select>
-
+                quantity
                 <div className="expand-quantity-counter">
-                       1
+                  <div className="img-icon" onClick={()=>setquality(quality > 1 ?  quality-1 : quality)}>
+                        <img src={minus}/>
+                  </div>
+                  <div className="expand-quantity"> {quality}</div>
+                  <div className="img-icon" onClick={()=>setquality(quality+1)}>
+                        <img src={plus}/>
+                  </div>
                 </div>
              </div>
           </div>
